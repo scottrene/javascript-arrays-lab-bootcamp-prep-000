475c083deb3878312017c89a65d78420e349a2d3
michele lafleur <mlafleurr@gmail.com>
2018-08-01 01:30:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,8 @@ function removeLastKitten() {
   var newKittens = kittens.slice(0,2);
   return newKittens
 }
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens
+}
